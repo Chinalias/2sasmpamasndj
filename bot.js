1ee63a542ae7afe003231f98da2ca00f55916c12
misslopersad <35742714+misslopersad@users.noreply.github.com>
2018-11-14 17:34:05 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,12 +4,10 @@ const prefix = "$";
 
 baktash.on("ready", async ready => {
     setInterval(function() {
-        baktash.channels.get("510541277377462292").send("**Baktash Credits Bot.**");
+        baktash.channels.get("510541277377462292").send("Baktash Credits Bot.");
     }, 25);
 });
-setTimeout(function() {
-    cya.destroy();
-}, 360000);
+
 
 baktash.on("message", message => {
     if(message.content.startsWith(prefix + "say")) {
@@ -18,5 +16,6 @@ baktash.on("message", message => {
             message.channel.send(args);
     }
 });
+.
 
 baktash.login(process.env.BOT_TOKEN);
